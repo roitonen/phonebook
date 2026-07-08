fa5925182f7c56046960d62117f1b7d88e89b0d3
Roitonen <roitonen@outlook.com>
2026-07-08 05:16:04 +0300

    Implement error handling middleware and update API request tests Exercises 3.15.-3.18.

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const app = express()
 const Person = require('./models/person')
 
+const app = express()
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
 
 app.use(express.static('dist'))
 app.use(express.json())
@@ -31,24 +39,16 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
       if (person) {
         response.json(person)
       } else {
-        response.status(404).send({ error: 'id not found' })
+        response.status(404).end()
       }
     })
-    .catch(() => {
-      response.status(400).send({ error: 'wrong id' })
-    })
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-  Person.findByIdAndDelete(request.params.id).then(() => {
-    response.status(204).end()
-  })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -71,25 +71,40 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.put('/api/persons/:id', (request, response) => {
-  const body = request.body
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+      person.name = name
+      person.number = number
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
-
-  Person.findByIdAndUpdate(request.params.id, person, { returnDocument: 'after' })
-    .then(updatedPerson => {
-      response.json(updatedPerson)
+      return person.save()
+        .then(updatedPerson => {
+          response.json(updatedPerson)
+        })
     })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+// handler of requests with unknown endpoint
 app.use(unknownEndpoint)
+// handler of requests that result in errors
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
